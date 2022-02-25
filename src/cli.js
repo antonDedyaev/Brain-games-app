9bd040646d12f15207e7ce5101fbf4bf83a54096
Aintdead86 <antondedyaev@gmail.com>
2022-02-25 16:50:22 +0300

    add cli.js with askName function

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const askName = () => {
+    const userName = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${userName}!`);
+};
+export default askName;
