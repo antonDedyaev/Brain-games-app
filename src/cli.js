8748592772baf70c384cd0946b9918df45b21dd2
Антон Дедяев <antondedyaev@gmail.com>
2022-02-27 22:18:41 +0300

    edit using eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const askName = () => {
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
 };
 export default askName;
