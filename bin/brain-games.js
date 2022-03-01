7cabf79ee4f2fd3e32a157ccefebed128de8344c
Антон Дедяев <antondedyaev@gmail.com>
2022-03-01 10:30:06 +0300

    edit brain-games script and module

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,4 @@
 
 import askName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
 askName();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
-  const userName = readlineSync.question('May I have your name?');
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 export default askName;
