c514e4594882a87d2bb7ad2192b087d5c8f757cf
Антон Дедяев <antondedyaev@gmail.com>
2022-03-09 20:14:30 +0300

    add calc function to calculate result of the expression

--- src/games/calc-logic.js
+++ src/games/calc-logic.js
@@ -1,35 +1,32 @@
 /* eslint-disable default-case */
-import commonLogic from '../index.js';
+import defineCommonLogic from '../index.js';
+import getRandomInt from '../utils.js';
 
 const gameTask = 'What is the result of the expression? ';
 
-const gameAnswers = () => {
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  const randomOperator = () => {
+const getAnswers = () => {
+  const getRandomOperator = () => {
     const operators = ['+', '-', '*'];
-    const i = getRandomInt(3);
+    const i = getRandomInt(0, 3);
     return operators[i];
   };
-  const randomNum1 = getRandomInt(100);
-  const randomNum2 = getRandomInt(100);
-  const operator = randomOperator(3);
-  const askQuestion = `${randomNum1} ${operator} ${randomNum2}`;
-  let result;
-  switch (operator) {
-    case '+':
-      result = randomNum1 + randomNum2;
-      break;
-    case '-':
-      result = randomNum1 - randomNum2;
-      break;
-    case '*':
-      result = randomNum1 * randomNum2;
-      break;
-  }
-  const referenceAnswer = String(result);
+  const randomNum1 = getRandomInt(0, 100);
+  const randomNum2 = getRandomInt(0, 100);
+  const randomOperator = getRandomOperator(0, 3);
+  const askQuestion = `${randomNum1} ${randomOperator} ${randomNum2}`;
+  const calc = (arrayOfNums, operator) => {
+    switch (operator) {
+      case '+':
+        return arrayOfNums[0] + arrayOfNums[1];
+      case '-':
+        return arrayOfNums[0] - arrayOfNums[1];
+      case '*':
+        return arrayOfNums[0] * arrayOfNums[1];
+    }
+    return undefined;
+  };
+  const referenceAnswer = String(calc([randomNum1, randomNum2], randomOperator));
   return [askQuestion, referenceAnswer];
 };
-const calculator = () => commonLogic(gameTask, gameAnswers);
-export default calculator;
+const calculateNums = () => defineCommonLogic(gameTask, getAnswers);
+export default calculateNums;
