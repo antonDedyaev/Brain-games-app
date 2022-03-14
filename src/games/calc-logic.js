7f99d039b7b6c20a20e516c1199128082ef9c6d0
Антон Дедяев <antondedyaev@gmail.com>
2022-03-14 22:45:05 +0300

    make getRandomOperator adaptable to operators array length

--- src/games/calc-logic.js
+++ src/games/calc-logic.js
@@ -1,21 +1,21 @@
 /* eslint-disable default-case */
-import defineCommonLogic from '../index.js';
+import runGame from '../index.js';
 import getRandomInt from '../utils.js';
 
 const getRandomOperator = () => {
   const operators = ['+', '-', '*'];
-  const i = getRandomInt(0, 3);
+  const i = getRandomInt(0, operators.length);
   return operators[i];
 };
 
-const calc = (Num1, Num2, operator) => {
+const calc = (num1, num2, operator) => {
   switch (operator) {
     case '+':
-      return Num1 + Num2;
+      return num1 + num2;
     case '-':
-      return Num1 - Num2;
+      return num1 - num2;
     case '*':
-      return Num1 * Num2;
+      return num1 * num2;
   }
   return null;
 };
@@ -24,11 +24,11 @@ const gameTask = 'What is the result of the expression? ';
 const getAnswers = () => {
   const randomNum1 = getRandomInt(0, 100);
   const randomNum2 = getRandomInt(0, 100);
-  const randomOperator = getRandomOperator(0, 3);
+  const randomOperator = getRandomOperator();
 
   const askQuestion = `${randomNum1} ${randomOperator} ${randomNum2}`;
   const referenceAnswer = String(calc(randomNum1, randomNum2, randomOperator));
   return [askQuestion, referenceAnswer];
 };
-const calculateNums = () => defineCommonLogic(gameTask, getAnswers);
+const calculateNums = () => runGame(gameTask, getAnswers);
 export default calculateNums;
